f02f9f9c49ebc41ce568d2a9255886d50248e31a
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-06-21 11:45:05 +0100

    add "channel" option

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 
 const imhist = require('ndarray-imhist');
 
-function entropy (ndarray) {
-  let [, y] = imhist(ndarray, {plot: false});
+const defaultOptions = {
+  channel: 0
+}
+
+function entropy (ndarray, options) {
+  options = Object.assign({}, defaultOptions, options);
+  validateOptions(ndarray, options);
+
+  let [, y] = imhist(ndarray, {...options, plot: false});
 
   let ySum = sum(y);
   let s = 0;
@@ -23,6 +30,13 @@ function sum (array) {
   return array.reduce((a, b) => {
     return a + b;
   }) 
+}
+
+function validateOptions (ndarray, options) {
+  const maxChannel = ndarray.shape[2] - 2;
+  if (options.channel && (options.channel > maxChannel || options.channel < 0)) {
+    throw new Error(`Channel should be in the interval [0-${maxChannel}]`)
+  }
 }
 
 module.exports = entropy;
